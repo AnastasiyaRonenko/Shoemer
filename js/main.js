ed66c54d31db45c47644f6caddd0312f650b09d2
AnastasiyaRonenko <ronenko94@gmail.com>
2018-05-09 22:49:50 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -12,11 +12,36 @@ $(function(){
     	$(".basket").appendTo(".header__bottomInner");
     }
 
+    if (windowWidth<992) {
+        $(".left__sidebar").appendTo(".modal");
+    }
+
     $('input,textarea').focus(function(){
        $(this).data('placeholder',$(this).attr('placeholder'))
        $(this).attr('placeholder','');
     });
     $('input,textarea').blur(function(){
         $(this).attr('placeholder',$(this).data('placeholder'));
+    });
+
+    $('#sort').niceSelect();
+
+    $(".accordion__itemTitle").on("click",function(){
+
+        var accordionItem = $(this).closest(".accordion__item");
+        var accordionItemSiblings = $(accordionItem).siblings(".accordion__item");
+        var accordionCheckboxWrap = $(accordionItemSiblings).children(".accordion__checkboxWrap");
+
+        $(accordionCheckboxWrap).hide();
+
+        $(this).siblings(".accordion__checkboxWrap").slideToggle();
+    });
+    
+    $(".openModelBtn").on("click",function(){
+        $(".modal").addClass("active");
+    });
+
+    $(".closeModelBtn").on("click",function(){
+        $(".modal").removeClass("active");
     });
 });
